f562bd2175edf610785c4de62f321d23eecd9af1
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2022-03-09 15:13:06 -0300

    ordenando strings e trabalhando com datas

--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
 	public static void main(String[] args) {
@@ -19,15 +20,31 @@ public class OrdenaStrings {
 			return 0;
 		});
 
-		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
-
+//		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+		palavras.sort(Comparator.comparing(s -> s.length()));
+		palavras.sort(Comparator.comparing(String::length));
+		
+		Function<String, Integer> funcao1 = String::length;
+		Function<String, Integer> funcao2 = s -> s.length();
+		
+		Function<String, Integer> funcao3 = new Function<String, Integer>() {
+			@Override
+			public Integer apply(String s) {
+				return s.length();
+			};
+		};
+		
+		Comparator<String> comparador = Comparator.comparing(funcao3);
+		palavras.sort(comparador);
+		
+		
 		System.out.println(palavras);
 
-		Consumer<String> impressor = s -> System.out.println(s);
+		Consumer<String> impressor = System.out::println;
 		palavras.forEach(impressor);
 		
-		palavras.forEach(s -> System.out.println(s));
-
+		palavras.forEach(System.out::println);
 	}
 }
 
